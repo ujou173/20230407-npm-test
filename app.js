16939505a65b7fab7fbe3e3e6bb46cbe6bec5a5c
sumin <ujou173@gmail.com>
2023-04-07 16:29:25 +0900

    요일 한국어로 변환해서 출력 성공

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const server = http.createServer(function(request, response){
   if (request.url === "/") {
     function timeFunc() {
       const now = new Date();
-      const currentTime = moment(now).format('YYYY년 MM월 HH시 mm분 ss초 dddd');
-      return currentTime;
+      const currentTime = moment(now).format('YYYY년 MM월 HH시 mm분 ss초 ');
+      const dayOfWeek = moment(now).format('dddd');
+      const krDayOfWeek = {
+        Monday: "월요일",
+        Tuesday: "화요일",
+        Wednsday: "수요일",
+        Thursday: "목요일",
+        Friday: "금요일",
+        Saturday: "월요일",
+        Sunday: "월요일",
+      }
+      return currentTime + krDayOfWeek[dayOfWeek];
     }
     let time = timeFunc();
     console.log(time);
